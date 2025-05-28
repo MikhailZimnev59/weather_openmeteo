81f8938c401ace7a8b6f67cbe718e6059731e55c
Mikhail_Z <m.zimnev@yandex.ru>
2025-05-28 11:29:06 +0300

    Pylint

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -1,9 +1,10 @@
+'''Маршруты приложения weather_app'''
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('stats/', views.stats, name='stats'), # Вызов страницы со статистикой количеств вызовов по городам
+    path('stats/', views.stats, name='stats'), # Вызов страницы со статистикой вызовов по городам
     path('history/', views.history, name='history'), # История обращений к системе прогноз погоды
     path('statsapi/', views.stats_api, name='stats_api'), # Статистика вызовов (API)
 ]
